cbb8488a8a2fdb34a272f32a5754c1b2c30f5e84
Anes Cehic <anes.cehic@gmail.com>
2019-04-17 12:09:48 +0200

    Initial commit

---
+++ webapitest/Areas/Identity/Data/webapitestUser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace webapitest.Areas.Identity.Data
+{
+    // Add profile data for application users by adding properties to the webapitestUser class
+    public class webapitestUser : IdentityUser
+    {
+    }
+}
