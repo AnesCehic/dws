860bd6ee1da88d2c4a91ed5ede98210b050cc872
Sanjin Sehic <sanjin.tr@hotmail.com>
2019-04-22 15:39:55 +0200

    Added implementation for channels

--- Chater/Controllers/ChannelController.cs
+++ Chater/Controllers/ChannelController.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using Chater.Models;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -13,15 +15,20 @@ namespace Chater.Controllers
     public class ChannelController : Controller
     {
         private ApplicationDbContext _context;
+        private UserManager<ApplicationUser> _userManager;
 
-        public ChannelController(ApplicationDbContext context)
+        public ChannelController(
+            ApplicationDbContext context,
+            UserManager<ApplicationUser> userManager
+            )
         {
             _context = context;
+            _userManager = userManager;
         }
 
         [HttpGet]
-        [Route("channels")]
-        public async Task<ActionResult<IEnumerable<Group>>> GetChannels()
+        [Route("{userId}/channels")]
+        public async Task<ActionResult<IEnumerable<Group>>> GetChannels(string userId)
         {
             return await _context.Groups.ToListAsync();
         }
@@ -41,13 +48,21 @@ namespace Chater.Controllers
         }
 
         [HttpPost]
-        [Route("channels/create")]
-        public async Task<ActionResult<Group>> CreateChannel([FromBody] Group group)
+        [Route("{userId}/channels/create")]
+        public async Task<ActionResult<Group>> CreateChannel(string userId, [FromBody] Group group)
         {
-            _context.Groups.Add(group);
+            var user = await _userManager.FindByIdAsync(userId);
+            _context.Groups.Add(new Group() { Name = group.Name, User = user });
             await _context.SaveChangesAsync();
-
             return Ok(group);
+            
+        }
+
+        [HttpGet]
+        [Route("channel/getAllUsers")]
+        public async Task<ActionResult<IEnumerable<ApplicationUser>>> GetAllUsers()
+        {
+            return await _context.Users.ToListAsync();
         }
 
         //popraviti
@@ -81,6 +96,32 @@ namespace Chater.Controllers
             await _context.SaveChangesAsync();
 
             return NoContent();
+        }
+
+        // zavrsiti
+        [HttpPost]
+        [Route("channel/{id}/addUsers")]
+        public async Task<ActionResult> AddUsersToGroup(string id, UserDto userDto)
+        {
+            Group group = await _context.Groups.FindAsync(id);
+            ApplicationUser user = await _userManager.FindByIdAsync(userDto.Id);
+
+            UserGroups userGroups = new UserGroups
+            {
+                User = user,
+                Group = group
+            };
+
+            _context.UserGroups.Add(userGroups);
+
+            await _context.SaveChangesAsync();
+            return Ok();
+        }
+
+        public class UserDto
+        {
+            [Required]
+            public string Id;
         }
     }
 }
